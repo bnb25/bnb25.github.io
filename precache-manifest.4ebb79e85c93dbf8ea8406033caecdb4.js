10e9c936454dad2b2dacddd006e727fa588725ac
DeFiDev <defidev2021@gmail.com>
2022-08-16 00:25:56 -0700

    test

---
+++ precache-manifest.4ebb79e85c93dbf8ea8406033caecdb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de48a6a309fb7ae7a9de",
+    "url": "/static/css/main.55b989f8.chunk.css"
+  },
+  {
+    "revision": "de48a6a309fb7ae7a9de",
+    "url": "/static/js/main.de48a6a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "2e526ed45d3a04dca2c7",
+    "url": "/static/css/2.b5f9b9a1.chunk.css"
+  },
+  {
+    "revision": "2e526ed45d3a04dca2c7",
+    "url": "/static/js/2.2e526ed4.chunk.js"
+  },
+  {
+    "revision": "75546b4f19dec6820c9bda05cee89abb",
+    "url": "/static/media/bg.75546b4f.png"
+  },
+  {
+    "revision": "d6e2bdeed07524b85c95afa100d0585e",
+    "url": "/static/media/bg2.d6e2bdee.png"
+  },
+  {
+    "revision": "93ccfa4a526cddef6ed382ab2e887146",
+    "url": "/index.html"
+  }
+];
